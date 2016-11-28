f047fcda194525bddaa2d26da43c1b5400aaed6c
Ox Cart <ox.to.a.cart@gmail.com>
2016-11-28 07:52:27 -0600

    Allowing use of explicit address with NewListenerAddr

--- listen.go
+++ listen.go
@@ -23,14 +23,19 @@ func Listen(addr, pkfile, certfile string) (net.Listener, error) {
 		return nil, fmt.Errorf("Unable to listen for connections at %s: %s\n", addr, err)
 	}
 
-	return NewListener(l, pkfile, certfile)
+	return NewListenerAddr(l, addr, pkfile, certfile)
 }
 
 // NewListener creates a TLS listener based on the given listener using the
 // private key and certificate PEM files at the given paths. If no files exists,
 // it creates a new key and self-signed certificate at those locations.
 func NewListener(l net.Listener, pkfile, certfile string) (net.Listener, error) {
-	addr := l.Addr().String()
+	return NewListenerAddr(l, l.Addr().String(), pkfile, certfile)
+}
+
+// NewListenerAddr is like NewListener but uses the specified addr to generate
+// the cert.
+func NewListenerAddr(l net.Listener, addr string, pkfile, certfile string) (net.Listener, error) {
 	host, _, err := net.SplitHostPort(addr)
 	if err != nil {
 		return nil, fmt.Errorf("Unable to split host and port for %v: %v\n", addr, err)
